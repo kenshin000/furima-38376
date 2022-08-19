5c44570d8ae87972311cf3a942d6845d073853ea
kenshin000 <keta.0829.a@gmail.com>
2022-08-20 00:05:48 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+
+  private
+
+  def message_params
+    params.require(:items).permit(:name, :item_explain, :price, :image).merge(user_id: current_user.id)
+  end
+
+  
 end
