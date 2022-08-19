72ee53216d6d50dcf757aac31433d4a22faccb91
kenshin000 <keta.0829.a@gmail.com>
2022-08-19 19:57:35 +0900

    ユーザー管理機能の実装　編集１

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          validates :nickname, presence: true
          validates :first_name, presence: true,format: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
          validates :last_name, presence: true, format: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-         validates :birthday, presence: true#, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :birthday, presence: true
          validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
          validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
          validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,11 +46,24 @@ RSpec.describe User, type: :model do
          @user.valid?
          expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it "passwordが半角英数字混合でないと登録できない" do
+      it "数字のみのパスワードでは登録できない" do
          @user.password = "000000"
+         @user.password_confirmation = '000000'
          @user.valid?
-         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is invalid")
+         expect(@user.errors.full_messages).to include("Password is invalid")
       end
+      it "英字のみのパスワードでは登録できない" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+     end
+     it "全角文字を含むパスワードでは登録できない" do
+      @user.password = "あ00000"
+      @user.password_confirmation = 'あ00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+   end
       it "passwordとpassword_confirmationが不一致では登録できない" do
          @user.password = '123456'
          @user.password_confirmation = '1234567'
