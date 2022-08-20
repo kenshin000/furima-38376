9c5d666e17916316f999bf4142ed2f1ff40e2ecd
kenshin000 <keta.0829.a@gmail.com>
2022-08-20 13:25:30 +0900

    ActiveHashを使った項目のビューの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,21 @@
 class ItemsController < ApplicationController
 
-  private
-
-  def message_params
-    params.require(:items).permit(:name, :item_explain, :price, :image).merge(user_id: current_user.id)
+  def index
   end
 
-  
+  def new
+    @itme = Item.new
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:items).permit(:name, :item_explain, :price, :image, :category_id, :situation_id,
+      :area_id, :send_day_id, :send_load_id,
+    ).merge(user_id: current_user.id)
+  end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,22 @@
 class Item < ApplicationRecord
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :item_explain, presence: true
+  validates :price, presence: true
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
+  validates :send_day_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
+  validates :send_load_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
+
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :area
+  belongs_to :send_load
+  belongs_to :send_day
 end
