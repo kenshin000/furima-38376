f73ede994990ff3720ad7f8322acb90944957650
kenshin000 <keta.0829.a@gmail.com>
2022-08-22 16:18:37 +0900

    商品詳細表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
-
   validates :image, presence: true
   validates :name, presence: true
   validates :item_explain, presence: true
-  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true}, format: { with: /\A[0-9]+\z/ }
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
-  validates :situation_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
-  validates :send_day_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
-  validates :send_load_id, numericality: { other_than: 1, message: "can't be blank"}, presence: true
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }, format: { with: /\A[0-9]+\z/ }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :send_day_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :send_load_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
 
   belongs_to :user
   has_one_attached :image
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true,format: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-         validates :last_name, presence: true, format: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-         validates :birthday, presence: true
-         validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-         validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :birthday, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
 
-
-         has_many :items
-        end
+  has_many :items
+end
