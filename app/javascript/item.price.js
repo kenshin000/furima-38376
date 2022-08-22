85d831081cc0852587f69cad955754453532a233
kenshin000 <keta.0829.a@gmail.com>
2022-08-22 14:06:58 +0900

    単体テストコード　編集２

--- app/javascript/item.price.js
+++ app/javascript/item.price.js
@@ -5,7 +5,6 @@ window.addEventListener('load', () => {
 
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML =  Math.floor(priceInput.value * 0.1 );
   const addPofitDom = document.getElementById("profit");
