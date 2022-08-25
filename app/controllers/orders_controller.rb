c1177f6fb3d312c799a602821a8b5a93ddf44c4e
kenshin000 <keta.0829.a@gmail.com>
2022-08-25 18:34:38 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
+    if current_user == @item.user
+      redirect_to root_path
+    end
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
+
     @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
@@ -14,7 +25,13 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.request(:order_address).permit(:post_code, :municipalities, :address_number, :building_name, :phone_number, :area_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_code, :municipalities, :address_number, 
+      :building_name, :phone_number, :area_id, 
+    ).merge(user_id: current_user.id, item_id:@item.id,)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,7 @@ class OrderAddress
 
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :post_code, :municipalities, :address_number,
-    :building_name, :phone_number, :area_id, :order_id
+    :building_name, :phone_number, :area_id
 
     with_options presence: true do
       validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -12,7 +12,6 @@ class OrderAddress
       validates :phone_number,format: {with:/\A\d{10,11}\z/}
       validates :post_code
       validates :user_id
-      validates :order_id
       validates :item_id
     end
 
@@ -20,6 +19,6 @@ class OrderAddress
       
       order = Order.create(item_id: item_id, user_id: user_id)
     
-      Address.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, address_number: address_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+      Address.create(post_code: post_code, area_id: area_id, municipalities: municipalities, address_number: address_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
     end
   end
