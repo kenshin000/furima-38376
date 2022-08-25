0cee2c9c3a8c6ab0f61a33b378b865759a3d3da6
kenshin000 <keta.0829.a@gmail.com>
2022-08-26 00:34:40 +0900

    購入単体テスト　準備

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    sleep 0.1 
+  end
+  describe '商品購入機能' do
+    context '商品購入できる場合' do
+      it '必要な値がすべてが存在すれば購入できる' do
+        expect(@order_address).to be_valid
+      end
+      it 'building_nameは空でも購入できる' do
+      end
+    end
+
+    context '商品購入できない場合' do
+      it 'post_codeが空では購入できない' do
+      end
+      it ' post_codeは、「3桁ハイフン4桁」の半角文字列でないと購入できない' do
+      end
+      it 'area_idが1では購入できない' do
+        @order_address.area_id = '1'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'municipalitiesが空では購入できない' do
+      end
+      it 'addressがnumberが空では購入できない' do
+      end
+      it 'phone_numberが空では購入できない' do
+      end
+      it 'phone_numberは10桁以上11桁以内の半角数値でないと購入できない' do
+      end
+    end
+
+
+  end
 end
